ebc57b91688e9b4455d31717b736c93e5dfcb50e
chiaradivece <chiara.divece.20@ucl.ac.uk>
2024-07-24 12:00:11 +0100

    Backup fv-dgx

--- lib/datasets/fetal.py
+++ lib/datasets/fetal.py
@@ -46,12 +46,13 @@ class FetalLandmarks(data.Dataset):
         self.anatomy = cfg.DATASET.ANATOMY
 
         # load annotations
-        self.landmarks_frame = pd.read_csv(self.csv_file, header=0)
+        self.landmarks_frame = pd.read_csv(self.csv_file, header=0, sep=',')
         self.landmarks_frame.drop(self.landmarks_frame.columns[0], axis=1, inplace=True)
         
         if is_train:
             if cfg.DATASET.ANATOMY == 'brain' or cfg.DATASET.ANATOMY == 'abdomen':
                 self.d_vect = determine_direction(np.array(self.landmarks_frame.iloc[1:, 9:13].values, dtype=np.float32))
+                print(f'd_vect: {self.d_vect}')
             else:
                 self.d_vect = determine_direction(np.array(self.landmarks_frame.iloc[1:, 5:9].values, dtype=np.float32))
 
@@ -77,12 +78,18 @@ class FetalLandmarks(data.Dataset):
             pts = self.landmarks_frame.iloc[idx, 9:13].values
         else:
             pts = self.landmarks_frame.iloc[idx, 5:9].values
+        
+        pts = pts[[1,0,3,2]]
         pts = pts.astype('float').reshape(-1, 2)
 
+        # Clip the points 
+        pts = np.clip(pts, a_min=0, a_max=None)
+
+        # print(f'pts: {pts}')
+        
         scale *= 1.7
         nparts = pts.shape[0]
         img = np.array(Image.open(image_path).convert('RGB'), dtype=np.float32)
-        # img = cv2.imread(image_path).astype(np.float32)
         # img = cv2.imread(image_path)  # Using OpenCV to read the image
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)  # Convert BGR to RGB
 
@@ -103,13 +110,15 @@ class FetalLandmarks(data.Dataset):
         tpts = pts.copy()
 
         for i in range(nparts):
-            if tpts[i, 1] > 0:
+            if tpts[i, 1] >= 0:
                 tpts[i, 0:2] = transform_pixel(tpts[i, 0:2]+1, center,
                                                scale, self.output_size, rot=r)
+                # print(f'tpts[i, 0:2]: {tpts[i, 0:2]}')
                 target[i] = generate_target(target[i], tpts[i]-1, self.sigma,
                                             label_type=self.label_type)
             else:
-                print ("ERROR!!!!!")
+                print ("ERROR HERE!!!!!")
+        
 
         # TODO: Remove
         if self.reassign :
@@ -121,8 +130,10 @@ class FetalLandmarks(data.Dataset):
             #     tmp = tpts[0, :].copy()
             #     tpts[0, :] = tpts[1, :]
             #     tpts[1, :] = tmp
+            print(f'shape of d_vect: {self.d_vect.shape}')
             tpts = classify_direction(tpts, self.d_vect)
-            target = np.zeros((nparts, self.output_size[0], self.output_size[1])) #We need to
+            print(f'tpts in self.reassign: {tpts}')
+            target = np.zeros((nparts, self.output_size[0], self.output_size[1])) 
             for i in range(nparts):
                 target[i] = generate_target(target[i], tpts[i] - 1, self.sigma,
                                             label_type=self.label_type)
@@ -131,11 +142,10 @@ class FetalLandmarks(data.Dataset):
         newimg = img.copy()
         # newimg[:, :, 1] = scipy.misc.imresize(target[0], (256, 256))
         # newimg[:, :, 2] = scipy.misc.imresize(target[1], (256, 256))
-        newimg[:, :, 1] = cv2.resize(target[0], (256, 256), interpolation=cv2.INTER_CUBIC)
-        newimg[:, :, 2] = cv2.resize(target[1], (256, 256), interpolation=cv2.INTER_CUBIC)
-        newimg = np.clip(newimg, 0, 255).astype(np.uint8)  # Ensure valid range and type for saving
+        newimg[:, :, 1] = cv2.resize(target[0]*255, (256, 256), interpolation=cv2.INTER_CUBIC)
+        newimg[:, :, 2] = cv2.resize(target[1]*255, (256, 256), interpolation=cv2.INTER_CUBIC)
         newimg = cv2.cvtColor(newimg, cv2.COLOR_RGB2BGR)  # Convert back to BGR for saving
-        cv2.imwrite(os.path.join(out_dir, "img{}_ttindex{}.png".format(idx, curidx)), newimg)
+        # cv2.imwrite(os.path.join(out_dir, "img{}_ttindex{}.png".format(idx, curidx)), newimg)
         globals()["curidx"] = curidx + 1
         img = (img / 255.0 - self.mean) / self.std
         img = img.transpose([2, 0, 1])
@@ -155,6 +165,8 @@ def determine_direction(pts_arr, do_plot = False):
         plt.scatter(pts_arr[::2,0], pts_arr[::2,1], alpha=1)
         plt.scatter(pts_arr[1::2,0], pts_arr[1::2,1], color='r', alpha=1)
         plt.plot(gmm.means_[:,0], gmm.means_[:,1], color='k')
+        # save plot
+        plt.savefig('/tmp/OutTrain/plot.png')
     
     return gmm.means_
 
